71618e146252753ce336bc8f97005ff4d7fdc9d6
Izere Moubarak <izeremubarak05@gmail.com>
2025-12-27 11:13:04 +0200

    added server test

---
+++ public/server_test.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Server Verification</h1>";
+echo "<p>PHP is working correctly.</p>";
+echo "<p>Current Directory: " . __DIR__ . "</p>";
+echo "<p>Next Step: If you can see this, the domain is pointing to the correct folder. If the main site (/) gives a 404, check if the hidden <code>.htaccess</code> file was uploaded.</p>";
